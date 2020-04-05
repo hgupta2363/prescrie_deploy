975a4239b59a729543fa33ebcae456256991d861
Mona Lisa <hgupta2363@gmail.com>
2020-04-05 14:21:49 +0530

    Updates

---
+++ precache-manifest.5eb44400fb1143a783e930123d89f867.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823fb4670f274a7921043b31f193fda7",
+    "url": "/prescrie_deploy/index.html"
+  },
+  {
+    "revision": "9986ce178cf7922320a7",
+    "url": "/prescrie_deploy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cc126051641abf045d36",
+    "url": "/prescrie_deploy/static/css/main.d90c3288.chunk.css"
+  },
+  {
+    "revision": "9986ce178cf7922320a7",
+    "url": "/prescrie_deploy/static/js/2.a2621c42.chunk.js"
+  },
+  {
+    "revision": "0348515facc10e9e9f2e179333751860",
+    "url": "/prescrie_deploy/static/js/2.a2621c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc126051641abf045d36",
+    "url": "/prescrie_deploy/static/js/main.29833147.chunk.js"
+  },
+  {
+    "revision": "0b11bd4e0d5dcc69ac27",
+    "url": "/prescrie_deploy/static/js/runtime-main.ee5cd5ce.js"
+  },
+  {
+    "revision": "32590a115ec8c61e2b0e35976f0ce300",
+    "url": "/prescrie_deploy/static/media/subscribe-bg.32590a11.png"
+  }
+]);
